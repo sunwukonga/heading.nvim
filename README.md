f1355c27e08f2764f4541f0cd5d217a095b756ce
Paul Desmond Parker <paul.parker@dcconnect.cn>
2025-07-12 17:16:16 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# headings.nvim
+---
+Create a comment heading from a selection not exceeding 64 characters in
+length.
+
+Example:
+```ts
+
+```
---
+++ lua/heading/init.lua
@@ -0,0 +1,47 @@
+local M = {}
+
+function M.add_heading()
+
+  local s = vim.fn.getpos("'<")
+  local e = vim.fn.getpos("'>")
+  local bufnr = vim.api.nvim_get_current_buf()
+  local l1, c1 = s[2], s[3]
+  local l2, c2 = e[2], e[3]
+
+  if l1 ~= l2 then
+    vim.notify('Selection can only be a single line!')
+    return
+  end
+
+  -- Get the selected lines
+  local lines = vim.api.nvim_buf_get_lines(bufnr, l1 - 1, l2, false)
+  local line = lines[1]
+  local selection = line:sub(c1, c2)
+  -- capture the inner run of characters after stripping spaces at both ends
+  local trimmed = selection:match("^%s*(.-)%s*$")
+  if (string.len(trimmed) > 64) then
+    vim.notify('Selection is too big!')
+    return
+  end
+  if (string.len(trimmed) < 1) then
+    vim.notify('Selection contains nothing!')
+    return
+  end
+
+  -- compute how many spaces to pad on the left
+  local spaces = (64 - string.len(trimmed)) / 2
+
+  -- build the output exactly as in the Rust example
+  local out_lines = {
+      "    /*//////////////////////////////////////////////////////////////",
+      "    " .. string.rep(" ", spaces) .. string.upper(trimmed),
+      "    //////////////////////////////////////////////////////////////*/"
+    }
+
+  -- Insert the result immediately before the selection
+  vim.api.nvim_buf_set_lines(
+    0, l1 - 1, l1 - 1, false, out_lines)
+
+end
+
+return M
